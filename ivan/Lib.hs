126b2a535bdf130436fa239c5134f7de6ba71a27
Ivan Kociman <ikociman@frba.utn.edu.ar>
2024-06-06 17:25:57 -0300

    Segundo commit

--- ivan/Lib.hs
+++ ivan/Lib.hs
@@ -1,7 +1,3 @@
--- Escribí tu código acá
--- Los autos se componen de marca, modelo, desgaste (ruedas y chasis, son dos números), velocidad máxima (m/s), y el tiempo de carrera, 
--- que lo vamos a considerar inicialmente 0 y tendremos en cuenta luego el uso.
-
 import Text.Show.Functions
 
 -------------
@@ -17,31 +13,13 @@ data Auto = Auto {
 } deriving (Show, Eq)
 
 ferrari :: Auto
-ferrari = Auto{
-    marca="Ferrari",
-    modelo="F50",
-    desgaste=(0,0), 
-    velocidadMaxima=65,
-    tiempoDeCarrera=0
-}
+ferrari = Auto "Ferrari" "F50" (0,0) 65 0
 
 lamborghini :: Auto
-lamborghini = Auto{
-    marca="Lamborghini",
-    modelo="Diablo",
-    desgaste=(7,4),
-    velocidadMaxima=73,
-    tiempoDeCarrera=0
-}
+lamborghini = Auto "Lamborghini" "Diablo" (7,4) 73 0
 
 fiat :: Auto
-fiat = Auto{
-    marca="Fiat",
-    modelo="600",
-    desgaste=(33,27),
-    velocidadMaxima=44,
-    tiempoDeCarrera=0
-}
+fiat = Auto "Fiat" "600" (33,27) 44 0
 
 -------------
 -- Punto 2 --
@@ -63,31 +41,91 @@ noDaMas unAuto = desgasteChasis unAuto > 80 || desgasteRuedas unAuto > 80
 -- Punto 3 --
 -------------
 
-cambiarDesgasteChasis :: (Float -> Float) -> Auto -> Auto
-cambiarDesgasteChasis modificador unAuto = unAuto {desgaste = (desgasteRuedas unAuto, (modificador)(desgasteChasis unAuto))}
+actualizarValorChasis :: Float -> Auto -> Auto
+actualizarValorChasis desgasteNuevo unAuto = unAuto { desgaste = (fst (desgaste unAuto), desgasteNuevo) }
 
-cambiarDesgasteRuedas :: (Float -> Float) -> Auto -> Auto
-cambiarDesgasteRuedas modificador unAuto = unAuto {desgaste = ((modificador)(desgasteRuedas unAuto), desgasteChasis unAuto)}
+actualizarValorRuedas :: Float -> Auto -> Auto
+actualizarValorRuedas desgasteNuevo unAuto = unAuto { desgaste = (desgasteNuevo, snd (desgaste unAuto)) }
+
+modificarChasis :: (Float -> Float) -> Auto -> Auto
+modificarChasis operacion unAuto = actualizarValorChasis (operacion (desgasteChasis unAuto)) unAuto
+
+modificarRuedas :: (Float -> Float) -> Auto -> Auto
+modificarRuedas operacion unAuto = actualizarValorRuedas (operacion (desgasteRuedas unAuto)) unAuto
 
 reparar :: Auto -> Auto
-reparar = cambiarDesgasteChasis (*0.15) . cambiarDesgasteRuedas (*0)
+reparar = modificarChasis (0.15 *) . actualizarValorRuedas 0
 
 
 -------------
 -- Punto 4 --
 -------------
 
---Una pista está compuesta de distintas partes (curvas, rectas, boxes), donde cada tramo termina realizando una transformación sobre 
---el auto que la atraviesa.
-
 modificarTiempo :: (Float -> Float) -> Auto -> Auto
-modificarTiempo modificador unAuto = unAuto {tiempoDeCarrera = (modificador)(tiempoDeCarrera unAuto)}
+modificarTiempo modificador unAuto = unAuto {tiempoDeCarrera = modificador (tiempoDeCarrera unAuto)}
 
-danioRuedaCurva :: Float -> Float -> Float
-danioRuedaCurva longitud angulo = (3 *) $ (/) longitud angulo
+sumarSegundos :: Float -> Auto -> Auto
+sumarSegundos segundos = modificarTiempo ((+) segundos)
 
 atravesarCurva :: Float -> Float -> Auto -> Auto
-atravesarCurva angulo longitud unAuto = cambiarDesgasteRuedas ((-) (danioRuedaCurva longitud angulo)) . modificarTiempo (+ (angulo/(velocidadMaxima unAuto)/2)) $ unAuto
+atravesarCurva angulo longitud unAuto = modificarRuedas ((-) $ 3 * longitud / angulo) . sumarSegundos (2 * longitud / velocidadMaxima unAuto) $ unAuto
+
+atravesarRecta :: Float -> Auto -> Auto
+atravesarRecta longitud unAuto = modificarChasis ((-) $ longitud / 100) . sumarSegundos (longitud / velocidadMaxima unAuto) $ unAuto
+
+curvaPeligrosa :: Auto -> Auto
+curvaPeligrosa = atravesarCurva 60 300
+
+curvaTranca :: Auto -> Auto
+curvaTranca = atravesarCurva 110 550
+
+tramoRectoClassic :: Auto -> Auto
+tramoRectoClassic = atravesarRecta 750
+
+tramito  :: Auto -> Auto
+tramito = atravesarRecta 280
+
+boxes :: Auto -> Auto
+boxes unAuto
+    | not.enBuenEstado $ unAuto = sumarSegundos 10 . reparar $ unAuto
+    | otherwise = unAuto
+
+valorAbsoluto :: Float -> Float
+valorAbsoluto valor
+    | valor < 0 = valor * (-1)
+    | otherwise = valor
+
+diferenciaDeTiempos :: Auto -> Auto -> Float
+diferenciaDeTiempos unAuto otroAuto = valorAbsoluto $ tiempoDeCarrera unAuto - tiempoDeCarrera otroAuto
+
+diferenciaChasis :: Auto -> Auto -> Float
+diferenciaChasis unAuto otroAuto = valorAbsoluto $ desgasteChasis unAuto - desgasteChasis otroAuto
+
+diferenciaRuedas :: Auto -> Auto -> Float
+diferenciaRuedas unAuto otroAuto = valorAbsoluto $ desgasteRuedas unAuto - desgasteRuedas otroAuto
+
+tramoMojado :: (Auto -> Auto) -> Auto -> Auto
+tramoMojado tramo unAuto = sumarSegundos ((diferenciaDeTiempos unAuto $ tramo unAuto)/2) unAuto
+
+--tieneRipo :: (Auto -> Auto) -> Auto -> Auto
+--tieneRipo tramo unAuto =  modificarRuedas ((-) $ 1) unAuto
+
+-- doble de tiempo
+-- doble de efecto
+
+-- Algunos tramos tienen ripio (sí... está un poco descuidada la pista) y produce el doble de efecto de un tramo normal equivalente, 
+-- y se tarda el doble en atravesarlo también. Nos aclaran que, por suerte, nunca hay boxes con ripio.
+
+
+{-
+modificarTiempo :: (Float -> Float) -> Auto -> Auto
+modificarTiempo modificador unAuto = unAuto {tiempoDeCarrera = modificador (tiempoDeCarrera unAuto)}
+
+danioRuedaCurva :: Float -> Float -> Float
+danioRuedaCurva longitud angulo = 3 * longitud / angulo
+
+atravesarCurva :: Float -> Float -> Auto -> Auto
+atravesarCurva angulo longitud unAuto = modificarRuedas (( - (3 * angulo))) . modificarTiempo (+ (angulo/(velocidadMaxima unAuto)/2)) $ unAuto
 
 curvaPeligrosa :: Auto -> Auto
 curvaPeligrosa = atravesarCurva 60 300
@@ -110,3 +148,4 @@ boxes True unAuto = modificarTiempo (+ 10) . reparar $ unAuto
 
 limpiezaLLuvia :: (Auto -> Auto) -> Auto -> Auto
 limpiezaLLuvia tramo unAuto =  modificarTiempo (+ ((tiempoDeCarrera (tramo unAuto)) - (tiempoDeCarrera unAuto))/2) unAuto
+-}
